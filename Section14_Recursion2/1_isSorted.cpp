315b2f81b3e9f024d40924ee3537ba81b4c5c2df
kangyul <kyul719@gmail.com>
2021-09-19 22:01:08 -0700

    Recursion Intermediate

---
+++ Section14_Recursion2/1_isSorted.cpp
@@ -0,0 +1,52 @@
+// Check if Array is Sorted
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool isSorted(int a[], int n) {
+
+    if(n == 0 || n == 1) return true;
+
+    if(a[0] > a[1]) return false;
+
+    bool isSmallerSorted = isSorted(a+1, n-1);
+
+    return isSmallerSorted;
+}
+
+bool isSorted2(int a[], int n) {
+    if(n==0 || n==1) return true;
+
+    bool isSmallerSorted = isSorted2(a+1, n-1);
+
+    if(!isSmallerSorted) {
+        return false;
+    } 
+
+    if(a[0] > a[1]) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+bool isSorted3(int a[], int n) {
+    if(n==0 || n==1) return true;
+
+    if(a[n-2] > a[n-1]) return false;
+
+    return isSorted3(a, n-1);
+}
+
+int main(void) {
+
+    int a[] = {1, 2, 3, 4, 5};
+    if(isSorted(a, 5)) {
+        cout << "Sorted" << endl;
+    } else {
+        cout << "Not Sorted" << endl;
+    }
+
+    return 0;
+}
