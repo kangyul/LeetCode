985b63546528e4230402cf4e0acf417910759093
kangyul <kyul719@gmail.com>
2021-09-22 14:20:07 -0700

    Check if Element is Present

--- Section14_Recursion2/3_checkIfElementIsPresent.cpp
+++ Section14_Recursion2/3_checkIfElementIsPresent.cpp
@@ -10,14 +10,6 @@ bool isPresent(int a[], int n, int x) {
     return isPresent(a+1, n-1, x);
 }
 
-bool isPresent(int a[], int n, int x) {
-    if(n==0) return false;
-
-    if(a[0] == x) return true;
-
-    return isPresent(a+1, n-1, x);
-}
-
 bool isPresent2(int a[], int n, int x) {
     if(n==0) return false;
 
@@ -29,11 +21,27 @@ bool isPresent2(int a[], int n, int x) {
     else return false;
 }
 
+bool isPresent3(int a[], int n, int x) {
+    if(n==0) return false;
+
+    if(a[n-1] == x) return true;
+
+    return isPresent3(a, n-1, x);
+}
+
+bool isPresent4(int a[], int n, int x, int i) {
+    if(i==n) return false;
+
+    if(a[i] == x) return true;
+
+    return isPresent4(a, n, x, i+1);
+}
+
 int main(void) {
 
     int arr[] = {1, 2, 3, 4, 5};
 
-    if(isPresent2(arr, 5, 6)) {
+    if(isPresent4(arr, 5, 3, 0)) {
         cout << "The element is in the array" << endl;
     } else {
         cout << "The element is not in the array" << endl;
