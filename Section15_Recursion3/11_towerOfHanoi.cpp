11977888b23d01a08a70e25e6db19c6dee6e6b0f
kangyul <kyul719@gmail.com>
2021-09-30 13:38:23 -0700

    Tower of Hanoi Steps

--- Section15_Recursion3/11_towerOfHanoi.cpp
+++ Section15_Recursion3/11_towerOfHanoi.cpp
@@ -10,12 +10,22 @@ int toh(int n) {
 
 // formula = 2^n - 1;
 
+void printSteps(int n, char s, char d, char h) {
+    if(n==0) return;
+
+    printSteps(n-1, s, h, d);
+    cout << "Moving disk " << n << " from " << s << " to " << d << endl;
+    printSteps(n-1, h, d, s);
+}
+
 int main(void) {
 
     int n;
     cin >> n;
 
     cout << toh(n) << endl;
+
+    printSteps(n, 'A', 'C', 'B');
 
     return 0;
 }
