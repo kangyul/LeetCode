c6f2394a64a6fbcd408b5aa0bb3545693d97e1ad
kangyul <kyul719@gmail.com>
2021-10-12 14:20:15 -0700

    Finr First & Last Occurence

---
+++ Section22_AdvancedBS/firstAndLastOccur.cpp
@@ -0,0 +1,17 @@
+// Leetcode 34. Find First and Last Position of Element in Sorted Array (Medium)
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main(void) {
+
+    int nums[] = {5,7,7,8,8,10};
+    int target = 8;
+
+
+
+    return 0;
+}
